016b29a9bfcf621d3c0cd263b9b0b9ad1ac75f3b
iz590985 <you@example.com>
2022-02-26 13:50:28 -0500

    Created the button styled components

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Screen, Previous, Current } from './Styled';
+import { Container, Screen, Previous, Current, Button } from './Styled';
 
 export default function Calculator() {
 	return (
@@ -8,6 +8,24 @@ export default function Calculator() {
 				<Previous></Previous>
 				<Current></Current>
 			</Screen>
+			<Button gridSpan={2}>AC</Button>
+			<Button control>DEL</Button>
+			<Button operation>÷</Button>
+			<Button>7</Button>
+			<Button>8</Button>
+			<Button>9</Button>
+			<Button operation>×</Button>
+			<Button>4</Button>
+			<Button>5</Button>
+			<Button>6</Button>
+			<Button operation>+</Button>
+			<Button>1</Button>
+			<Button>2</Button>
+			<Button>3</Button>
+			<Button operation>-</Button>
+			<Button period>.</Button>
+			<Button>0</Button>
+			<Button gridSpan={2}>=</Button>
 		</Container>
 	)
 }
